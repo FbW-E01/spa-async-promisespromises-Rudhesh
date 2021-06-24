a368e42a7206147edf516e0bc391fdc63be55d27
dci <rudeshdhote@gmail.com>
2021-06-24 14:35:54 +0200

    finish

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -5,3 +5,33 @@ import '../styles/main.scss';
 
 // \/ All of your javascript should go here \/
 
+var square = (x) => {
+    return x * x;
+  };
+  
+  console.log(square(5));
+
+  const modal = document.querySelector(".modal")
+
+  const p = new Promise((resolve, reject)=> {
+      setTimeout(()=> {
+        resolve( "yay")
+       return modal.style.display = "block"
+      }, 60000)
+
+      document.querySelector(".close").addEventListener("click", ()=> {
+        modal.style.display = ""
+      })
+   
+   
+  } )
+
+  p.then((result) => {
+      console.log("done", result)
+  })
+  p.catch((error) => {
+      console.loh("done", error)
+  })
+  p.finally(()=> {
+    console.log("finally")
+})
